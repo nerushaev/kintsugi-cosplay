83753ade3ac2d10e4fdca0551248926cec5ae6a5
Nikolay Chereliuk <kolyanerushaev@gmail.com>
2022-07-22 00:47:59 +0300

    1

---
+++ src/js/swiper.js
@@ -0,0 +1,14 @@
+  import Swiper from 'swiper';
+  import '/node_modules/swiper/swiper-bundle.min.css';
+  
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
